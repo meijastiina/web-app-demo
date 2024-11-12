b8b264a00e67cfb017540b414a0492f2d801533e
Meija Lohiniva <meijastiina@gmail.com>
2024-11-12 09:31:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello from backend root!');
+})
+
+app.get('/api/message', (req, res) =>{
+    res.json({ message: 'This is a message from the backend.'});
+})
+app.listen(port);
